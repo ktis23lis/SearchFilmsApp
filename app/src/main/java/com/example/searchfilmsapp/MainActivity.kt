5e623dc5b3155c41cc8b9d74d8e0809437e970a9
kris23lis <kris23lis@gmail.com>
2021-04-30 12:24:42 +0300

    Search Films App

---
+++ app/src/main/java/com/example/searchfilmsapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.searchfilmsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.searchfilmsapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
